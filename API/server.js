df7d5240a09fb81ed9b4e0d033fe7e07f576ca2a
githusavy <githusavy1996@gmail.com>
2023-06-26 13:37:42 +0100

    added docker support

--- API/server.js
+++ API/server.js
@@ -43,55 +43,6 @@ app.use('/air-quality', airQualityRoutes);
 app.use('/history', historyRoutes);
 app.use('/cities', cityRoutes);
 
-//   try {
-//     const { city, day } = req.query;
-//     if (!day) {
-//       day = 1;
-//     }
-//     // Check if the city is valid
-//     if (!stateCapitals.cities.includes(city)) {
-//       return res.status(400).json({ error: "Invalid city name" });
-//     }
-
-//     // Retrieve geolocation data using Open Meteo Geocoding API
-//     const geocodingUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`;
-//     const geocodingResponse = await axios.get(geocodingUrl);
-//     const geocodingData = geocodingResponse.data;
-
-//     // Check if geolocation data is available
-//     if (!geocodingData || geocodingData.length === 0) {
-//       throw new Error("Failed to retrieve geolocation data");
-//     }
-
-//     // Extract latitude and longitude from the result
-//     const { latitude, longitude } = geocodingData.results[0];
-
-//     // Prepare the URL for weather data retrieval
-//     const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m&daily=sunrise,sunset&forecast_days=${day}&timezone=auto`;
-
-//     // Retrieve weather data using Axios
-//     const weatherResponse = await axios.get(weatherUrl);
-//     const weatherData = weatherResponse.data;
-
-//     // Save the lookup history in the database
-//     await Lookup.create({ city });
-
-//     // Send the weather data as the response
-//     res.json(weatherData);
-//   } catch (error) {
-//     console.error("Error retrieving weather data", error);
-//     if (error.response && error.response.status === 400) {
-//       res.status(400).json({ error: "Invalid city name" });
-//     } else {
-//       res.status(500).json({ error: "Failed to retrieve weather data" });
-//     }
-//   }
-// });
-
-
-
-
-
 // Start the server
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
